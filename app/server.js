83a623e59200cbcf8f9ec548613627f8f0a1c766
srushtikhatale45-sketch <srushtikhatale45@gmail.com>
2026-04-14 13:43:09 +0530

    New Frotend

--- app/server.js
+++ app/server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 5000;
 
 const allowedOrigins = [
   'http://localhost:5173',
-  'https://otplessfrontendfinal.vercel.app'
+  'https://otpfrontend-sigma.vercel.app'
 ];
 
 app.use(cors({
